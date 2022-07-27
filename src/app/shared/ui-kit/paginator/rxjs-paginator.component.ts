acd222a23cc89c32bd4be4b386731f1718c49a87
Fabio Portieri <fabio.portieri@datamanagementitalia.it>
2022-07-27 17:52:22 +0200

    paginator part2

--- src/app/shared/ui-kit/paginator/rxjs-paginator.component.ts
+++ src/app/shared/ui-kit/paginator/rxjs-paginator.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { combineLatest, BehaviorSubject, Observable, of } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
 import { BooksWithAuthor } from 'src/app/books/books.model';
 
 @Component({
   selector: 'app-rxjs-paginator',
   template: `
-    <div class="paginator">
-      <div class="first"><<</div>
-      <div class="back"><</div>
+    <div class="paginator" *ngIf="(currentPage$ | async) as currentPage">
+      <div class="first" (click)="changePage(1)"><<</div>
+      <div class="back" (click)="changePage(currentPage-1)"><</div>
       <div *ngFor="let page of pages$ | async">
-        {{ page }}
+        <div class="pages" (click)="changePage(page)"> {{ page }} </div>
       </div>
-      <div class="next">>></div>
-      <div class="last">></div>
+      <div class="next" (click)="changePage(currentPage+1)"> >> </div>
+      <div class="last" (click)="changePage(getLastPage())"> > </div>
     </div>
 
     <div class="form">
@@ -24,20 +24,36 @@ import { BooksWithAuthor } from 'src/app/books/books.model';
       </button>
     </div>
 
-<!--
+
     <pre> number of pages: {{this.pagesNumber$ | async }} </pre>
     <pre> pages: {{this.pages$ | async | json }} </pre>
     <pre> elementsPerpages: {{this.elementsPerPage$ | async }} </pre>
-    <pre> totalLength: {{this.totalLength$ | async }} </pre> -->
+    <pre> totalLength: {{this.totalLength$ | async }} </pre>
+    <pre> currentPage: {{ currentPage$ | async }} </pre>
+
 
   `,
   styles: [`
   .paginator {
     display: flex;
-    gap: 8px;
+    gap: 10px;
+    background: #ededed;
+    padding: 5px;
+    justify-content: center;
   }
   .paginator > div {
-
+    width: 20px;
+    height: 20px;
+    cursor: pointer;
+    line-height: 20px;
+    border: 1px solid #ddd;
+    background: white;
+    text-align: center;
+    padding: 5px;
+    border-radius: 4px;
+    font-weight: 600;
+    color: #505050;
+    font-family: monospace;
   }
   `],
   encapsulation: ViewEncapsulation.None,
@@ -48,7 +64,8 @@ export class RxjsPaginatorComponent implements OnInit {
   pagesNumber$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   pages$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
   elementsPerPage$: BehaviorSubject<number> = new BehaviorSubject<number>(2);
-  totalLength$: Observable<number> = of(0);
+  currentPage$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  totalLength$: BehaviorSubject<number> = new BehaviorSubject<number>(0); //Observable<number> = of(0);
 
   elementsPerPageFC: FormControl = new FormControl();
 
@@ -83,11 +100,22 @@ export class RxjsPaginatorComponent implements OnInit {
   changeElementsPerPage(elementsPerPage: number): void {
     this.elementsPerPage$.next(elementsPerPage);
   }
+  changePage(page: number): void {
+    console.log('changePage called with page ', page);
+    if (page >= 1 && page < this.getLastPage()) {
+      console.log('goto page ', page);
+      this.currentPage$.next(page);
+    }
+  }
+  getLastPage(): number {
+    return this.pagesNumber$.getValue();
+  }
 
   private initPaginator(): void {
       // calculate length and pages
-    this.totalLength$ = of(this.data.length);
-
+    this.totalLength$.next(this.data.length);
+    this.totalLength$.subscribe(d => console.log('total length changed??', d))
+    this.currentPage$.next(1);
     // pages$: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
     /*
     // pages$: dipende da totalLength e pageNumber e elementsPerPage.
@@ -102,20 +130,21 @@ export class RxjsPaginatorComponent implements OnInit {
         const num = Math.ceil(length / elementsPerPage);
         this.pagesNumber$.next(num);
       })
-    ).subscribe();
+      ).subscribe(data => console.log('triggered change on length or elementsPerPage:', data));
 
-    this.pagesNumber$.pipe(
-      tap((pagesNumber: number) => {
+    combineLatest([this.currentPage$, this.totalLength$, this.elementsPerPage$]).pipe(
+      distinctUntilChanged(),
+      tap(([currentPage, length, elementsPerPage]: [number, number, number]) => {
 
-       const prevPage = (pagesNumber-1 > 0) ? pagesNumber-1 : null;
-       const prevPrevPage = (pagesNumber-2 > 0) ? pagesNumber-2 : null;
-       const nextPage = (pagesNumber+1 > 0) ? pagesNumber+1 : null;
-       const nextNextPage = (pagesNumber+2 > 0) ? pagesNumber+2 : null;
-       const pages = [prevPrevPage, prevPage, pagesNumber, nextPage, nextNextPage].filter(el => !!el) as number[];
+       const prevPage = (currentPage-1 > 0) ? currentPage-1 : null;
+       const prevPrevPage = (currentPage-2 > 0) ? currentPage-2 : null;
+       const nextPage = ((currentPage+1) * elementsPerPage <= length) ? currentPage+1 : null;
+       const nextNextPage = ((currentPage+2) * elementsPerPage <= length) ? currentPage+2 : null;
+       const pages = [prevPrevPage, prevPage, currentPage, nextPage, nextNextPage].filter(el => !!el) as number[];
 
        this.pages$.next(pages);
 
       })
-    ).subscribe();
+    ).subscribe(data => console.log('triggered change on currentpage length or elementsPerPage:', data));
   }
 }
